ab6463dc7b6d386d76aca1ec6d91eaef013a5b9c
Alexey Kotcov <richyshark@gmail.com>
2022-05-12 23:36:40 +0300

    minor

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 
   return (
       <div className="App">
-          <BrowserRouter>
+          <BrowserRouter basename="/fortnite-site">
               <Routes>
                   <Route path="/" element={
                           <Picker/>
